99654ef64b4a6bfc79b4a925a1e65995fa70c544
anuj26298 <kumar26298anuj@gmail.com>
2021-06-03 21:03:55 +0530

    ADDED code for maximum among 3 numbers, including testcase

--- src/main/java/com/generics/MaximumNumber.java
+++ src/main/java/com/generics/MaximumNumber.java
@@ -1,4 +1,14 @@
 package com.generics;
 
 public class MaximumNumber {
+    public static Integer maximumInteger(Integer number1, Integer number2, Integer number3) {
+        Integer maximum = number1;
+        if (number2.compareTo(maximum) > 0)
+            maximum = number2;
+        if (number3.compareTo(maximum) > 0)
+            maximum = number3;
+
+        return maximum;
+
+    }
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -1,3 +1,24 @@
+import com.generics.MaximumNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MaximumNumberTest {
+    @Test
+    void givenThreeInteger_WithMaxAtFirstPlace_RetrunMaximum() {
+        Integer maximum = MaximumNumber.maximumInteger(3,2,1);
+        Assertions.assertEquals(3,maximum);
+    }
+
+    @Test
+    void givenThreeeInteger_WithMaxAtSecondPlace_ReturnMaximum() {
+        Integer maximum = MaximumNumber.maximumInteger(2,3,1);
+        Assertions.assertEquals(3,maximum);
+    }
+
+    @Test
+    void givenThreeInteger_WithMaxAtThirdPlace_ReturnMaximum() {
+
+        Integer maximum = MaximumNumber.maximumInteger(1,2,3);
+        Assertions.assertEquals(3,maximum);
+    }
 }
