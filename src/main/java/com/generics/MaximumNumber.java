39ff5e94f77ea58d81bfca06b7df49af9cff85b7
anuj26298 <kumar26298anuj@gmail.com>
2021-06-03 21:31:54 +0530

    ADDED code for maximum of three float number, including testcases

--- src/main/java/com/generics/MaximumNumber.java
+++ src/main/java/com/generics/MaximumNumber.java
@@ -11,4 +11,14 @@ public class MaximumNumber {
         return maximum;
 
     }
+
+    public static Float maximumFloat(Float number1, Float number2, Float number3) {
+        Float maximum = number1;
+        if (number2.compareTo(maximum) > 0)
+            maximum = number2;
+        if (number3.compareTo(maximum) > 0)
+            maximum = number3;
+        return maximum;
+    }
+
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -4,21 +4,40 @@ import org.junit.jupiter.api.Test;
 
 public class MaximumNumberTest {
     @Test
-    void givenThreeInteger_WithMaxAtFirstPlace_RetrunMaximum() {
-        Integer maximum = MaximumNumber.maximumInteger(3,2,1);
-        Assertions.assertEquals(3,maximum);
+    void givenThreeInteger_WithMaxAtFirstPlace_ReturnMaximum() {
+        Integer maximum = MaximumNumber.maximumInteger(3, 2, 1);
+        Assertions.assertEquals(3, maximum);
     }
 
     @Test
-    void givenThreeeInteger_WithMaxAtSecondPlace_ReturnMaximum() {
-        Integer maximum = MaximumNumber.maximumInteger(2,3,1);
-        Assertions.assertEquals(3,maximum);
+    void givenThreeInteger_WithMaxAtSecondPlace_ReturnMaximum() {
+        Integer maximum = MaximumNumber.maximumInteger(2, 3, 1);
+        Assertions.assertEquals(3, maximum);
     }
 
     @Test
     void givenThreeInteger_WithMaxAtThirdPlace_ReturnMaximum() {
 
-        Integer maximum = MaximumNumber.maximumInteger(1,2,3);
-        Assertions.assertEquals(3,maximum);
+        Integer maximum = MaximumNumber.maximumInteger(1, 2, 3);
+        Assertions.assertEquals(3, maximum);
     }
+
+    @Test
+    void givenThreeFloat_WithMaxAtFirstPlace_ReturnMaximum() {
+        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.2f, 3.1f);
+        Assertions.assertEquals(3.3f, maximum);
+    }
+
+    @Test
+    void givenThreeFloat_WithMaxAtSecondPlace_ReturnMaximum() {
+        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.4f, 3.1f);
+        Assertions.assertEquals(3.4f, maximum);
+    }
+
+    @Test
+    void givenThreeFloat_WithMaxAtThirdPlace_ReturnMaximum() {
+        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.2f, 3.5f);
+        Assertions.assertEquals(3.5f, maximum);
+    }
+
 }
