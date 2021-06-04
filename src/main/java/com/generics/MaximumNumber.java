684030ae6ff6a65d6665bd03cf38e12351175d0d
anuj26298 <kumar26298anuj@gmail.com>
2021-06-04 10:34:37 +0530

    REFACTORED code to generic type methods"

--- src/main/java/com/generics/MaximumNumber.java
+++ src/main/java/com/generics/MaximumNumber.java
@@ -1,8 +1,8 @@
 package com.generics;
 
 public class MaximumNumber {
-    public static Integer maximumInteger(Integer number1, Integer number2, Integer number3) {
-        Integer maximum = number1;
+    public static <E extends  Comparable<E>> E maximum(E number1, E number2, E number3) {
+        E maximum = number1;
         if (number2.compareTo(maximum) > 0)
             maximum = number2;
         if (number3.compareTo(maximum) > 0)
@@ -11,23 +11,4 @@ public class MaximumNumber {
         return maximum;
 
     }
-
-    public static Float maximumFloat(Float number1, Float number2, Float number3) {
-        Float maximum = number1;
-        if (number2.compareTo(maximum) > 0)
-            maximum = number2;
-        if (number3.compareTo(maximum) > 0)
-            maximum = number3;
-        return maximum;
-    }
-
-    public static String maximumString(String str1, String str2, String str3) {
-        String maximum = str1;
-        if (str2.compareTo(maximum) > 0)
-            maximum = str2;
-        if (str3.compareTo(maximum) > 0)
-            maximum = str3;
-        return maximum;
-    }
-
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -5,57 +5,57 @@ import org.junit.jupiter.api.Test;
 public class MaximumNumberTest {
     @Test
     void givenThreeInteger_WithMaxAtFirstPlace_ReturnMaximum() {
-        Integer maximum = MaximumNumber.maximumInteger(3, 2, 1);
-        Assertions.assertEquals(3, maximum);
+        Integer maxInt = MaximumNumber.maximum(3, 2, 1);
+        Assertions.assertEquals(3, maxInt);
     }
 
     @Test
     void givenThreeInteger_WithMaxAtSecondPlace_ReturnMaximum() {
-        Integer maximum = MaximumNumber.maximumInteger(2, 3, 1);
-        Assertions.assertEquals(3, maximum);
+        Integer maxInt = MaximumNumber.maximum(2, 3, 1);
+        Assertions.assertEquals(3, maxInt);
     }
 
     @Test
     void givenThreeInteger_WithMaxAtThirdPlace_ReturnMaximum() {
 
-        Integer maximum = MaximumNumber.maximumInteger(1, 2, 3);
-        Assertions.assertEquals(3, maximum);
+        Integer maxInt = MaximumNumber.maximum(1, 2, 3);
+        Assertions.assertEquals(3, maxInt);
     }
 
     @Test
     void givenThreeFloat_WithMaxAtFirstPlace_ReturnMaximum() {
-        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.2f, 3.1f);
-        Assertions.assertEquals(3.3f, maximum);
+        Float maxFloat = MaximumNumber.maximum(3.3f, 3.2f, 3.1f);
+        Assertions.assertEquals(3.3f, maxFloat);
     }
 
     @Test
     void givenThreeFloat_WithMaxAtSecondPlace_ReturnMaximum() {
-        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.4f, 3.1f);
-        Assertions.assertEquals(3.4f, maximum);
+        Float maxFloat = MaximumNumber.maximum(3.3f, 3.4f, 3.1f);
+        Assertions.assertEquals(3.4f, maxFloat);
     }
 
     @Test
     void givenThreeFloat_WithMaxAtThirdPlace_ReturnMaximum() {
-        Float maximum = MaximumNumber.maximumFloat(3.3f, 3.2f, 3.5f);
-        Assertions.assertEquals(3.5f, maximum);
+        Float maxFloat = MaximumNumber.maximum(3.3f, 3.2f, 3.5f);
+        Assertions.assertEquals(3.5f, maxFloat);
     }
 
     @Test
     void givenThreeString_WithMaxAtFirst_ReturnMaximum() {
-        String maximum = MaximumNumber.maximumString("abcd", "abc", "ab");
-        Assertions.assertEquals("abcd", maximum);
+        String maxString = MaximumNumber.maximum("abcd", "abc", "ab");
+        Assertions.assertEquals("abcd", maxString);
     }
 
     @Test
     void givenThreeString_WithMaxAtSecond_ReturnMaximum() {
-        String maximum = MaximumNumber.maximumString("abc", "abcd", "ab");
-        Assertions.assertEquals("abcd", maximum);
+        String maxString = MaximumNumber.maximum("abc", "abcd", "ab");
+        Assertions.assertEquals("abcd", maxString);
     }
 
     @Test
     void givenThreeString_WithMaxAtThird_ReturnMaximum() {
-        String maximum = MaximumNumber.maximumString("abcd", "abc", "abcdef");
-        Assertions.assertEquals("abcdef", maximum);
+        String maxString = MaximumNumber.maximum("abcd", "abc", "abcdef");
+        Assertions.assertEquals("abcdef", maxString);
     }
 
 }
